670477ded6823f1d292021abd69a78a892eae751
Iago Guedelha Nunes <iagognunes@outlook.com>
2021-03-23 10:41:31 -0300

    update Aula 01
    Feito na Aula 01

---
+++ hacker-chat/client/src/constants.js
@@ -0,0 +1,14 @@
+export const constants = {
+    events: {
+        app:{
+            MESSAGE_RECEIVED: 'message:received',
+            MESSAGE_SENT: 'message:sent',
+            ACTIVITYLOG_UPDATED: 'activityLog:update',
+            STATUS_UPDATED: 'status:updated'
+        },
+
+        socket: {
+
+        },
+    }
+}
